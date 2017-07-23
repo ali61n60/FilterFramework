570166b7780b5491ae1b01450cd308103d87bb15
ali2 <ali62n62@yahoo.com>
2017-07-23 16:04:09 +0430

    init project

---
+++ FilterFramework/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilterFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Message", "This is the Index action on the Home controller");
+        }
+        public ViewResult SecondAction() => View("Message",
+            "This is the SecondAction action on the Home controller");
+    }
+
+
+}
